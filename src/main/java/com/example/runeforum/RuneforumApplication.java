13b6408c7e969d1ad506d64f4a48a42c63c33a59
Dylan <74596681+CBOnyx@users.noreply.github.com>
2022-02-21 10:38:07 +0100

    startup

---
+++ src/main/java/com/example/runeforum/RuneforumApplication.java
@@ -0,0 +1,15 @@
+package com.example.runeforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuneforumApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RuneforumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/runeforum/RuneforumApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.runeforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuneforumApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
